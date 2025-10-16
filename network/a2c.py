4b299deb03301ef4a82a11dd8e1ebe7cbebc916d
KienLee <117734973+kienle141204@users.noreply.github.com>
2025-10-16 18:53:13 +0700

    +a2c

---
+++ network/a2c.py
@@ -0,0 +1,34 @@
+import torch 
+from torch import nn
+import torch.nn.functional as F
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Actor, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(state_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, action_dim),
+            nn.Softmax(dim=-1)
+        )
+    def forward(self, x):
+        return self.fc(x)
+    
+class Critic(nn.Module):
+    def __init__(self, state_dim):
+        super(Critic, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(state_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+    def forward(self, x):
+        return self.fc(x)
